68be26d42955113c7c7a27653fc4a4074e79ef95
yannsy <yanshui177@gmail.com>
2018-06-26 21:22:34 +0800

    add save and show functions
    Signed-off-by: yannsy <yanshui177@gmail.com>

--- test1_load_image.py
+++ test1_load_image.py
@@ -13,21 +13,19 @@ import os
 
 class data:
     def __init__(self, addr):
-        self.img = cv2.imread(addr+'.jpg')
-        pos_num, pos = self.read_pts(addr=addr)
-        x1, y1, x2, y2, pos = self.get_margin(pos)
-        face = self.get_face(self.img, x1, y1, x2, y2)
-        mask = self.create_mask(pos)
+        self.addr = addr
 
-        cv2.imshow('11', face)
-        cv2.imshow('2211', mask)
-        cv2.imwrite('face.jpg', face)
-        cv2.imwrite('mask.jpg', mask)
+        img, pos = self.load_img_and_pts()
+        x1, y1, x2, y2, pos_small = self.get_margin(pos)
+        face = self.get_face(img, x1, y1, x2, y2)
+        mask = self.get_mask(pos_small)
+        self.save_img(face, mask)
 
-        cv2.waitKey(0)
-
-    def read_pts(self, addr):
-        f2 = open(addr+'.pts', "r")
+    def load_img_and_pts(self):
+        # load img
+        img = cv2.imread(self.addr + '.jpg')
+        # load pts
+        f2 = open(self.addr+'.pts', "r")
         lines = f2.readlines()
         pos_num = 0
         pos = []
@@ -42,7 +40,7 @@ class data:
                 break
             if flag:
                 pos.append([int(round(float(line3.split(' ')[0]))), int(round(float(line3.split(' ')[1])))])
-        return pos_num, np.array(pos)
+        return img, np.array(pos)
 
     def get_margin(self, pos):
         x2, y2 = np.max(pos, 0)
@@ -55,14 +53,25 @@ class data:
         width = x2 - x1
         return img[y1:y1 + hight, x1:x1 + width]
 
-    def create_mask(self, pos):
+    def get_mask(self, pos):
         width, height = np.max(pos, 0)
         img = np.zeros((height, width, 3), dtype=np.uint8)
         for i in range(len(pos)):
             cv2.circle(img, (pos[i][0], pos[i][1]), 2, (255, 255, 255), thickness=-1)
         return img
 
+    def save_img(self, img_face, img_mask):
+        path_img_face = 'D:/created_data/' + self.addr.split('/')[-1] + '_face.jpg'
+        path_img_mask = 'D:/created_data/' + self.addr.split('/')[-1] + '_mask.jpg'
+        cv2.imwrite(path_img_face, img_face)
+        cv2.imwrite(path_img_mask, img_mask)
+
+    def show_img(self, img_face, img_mask):
+        cv2.imshow('1', img_face)
+        cv2.imshow('2', img_mask)
+        cv2.waitKey(0)
 
 if __name__ == "__main__":
     addr = "./data/261068_2"
     data1 = data(addr)
+
